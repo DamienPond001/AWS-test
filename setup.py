ba3bfa6002781b6c4e7966f992f2e973876f83f5
Damien <pond@live.co.za>
2020-12-08 14:23:55 +0200

    v1.0.5 - env vars added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="voyclib",
-    version="1.0.4",
+    version="1.0.5",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
